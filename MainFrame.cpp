76f4965726b4de153bd04e7477dc0a3e3e8b9aed
Joseph Lavoie <josephlavoie43210@gmail.com>
2024-12-27 15:46:14 -0400

    Created a layout. Added Listbox, title, description, and quantity. Added fake items.

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -11,10 +11,88 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
     int width = screenSize.GetWidth() * 0.6;
     int height = screenSize.GetHeight() * 0.6;
 
-    // Set size
+    // Set size, START of MainFrame
     SetSize(width, height);
 
-    // Enable persistence
+    // Create main sizer
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxHORIZONTAL);
+
+    // Create left panel with listbox (20% width)
+    wxPanel* leftPanel = new wxPanel(this);
+    wxBoxSizer* leftSizer = new wxBoxSizer(wxVERTICAL);
+
+    // Create and populate listbox
+    m_listBox = new wxListBox(leftPanel, wxID_ANY);
+    m_listBox->Append("Sword");
+    m_listBox->Append("Shield");
+    m_listBox->Append("Potion");
+    m_listBox->Append("Bow");
+    m_listBox->Append("Arrow");
+
+    leftSizer->Add(m_listBox, 1, wxEXPAND | wxALL, 5);
+    leftPanel->SetSizer(leftSizer);
+    mainSizer->Add(leftPanel, 20, wxEXPAND);
+
+    // Create right panel (80% width)
+    wxPanel* rightPanel = new wxPanel(this);
+    wxBoxSizer* rightSizer = new wxBoxSizer(wxVERTICAL);
+
+    // Add counter
+    m_itemCount = new wxStaticText(rightPanel, wxID_ANY, "Quantity: 0");
+    rightSizer->Add(m_itemCount, 0, wxALL, 5);
+
+    // Add title
+    m_itemTitle = new wxStaticText(rightPanel, wxID_ANY, "Select an item", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER);
+    wxFont titleFont = m_itemTitle->GetFont();
+    titleFont.SetPointSize(14);
+    titleFont.SetWeight(wxFONTWEIGHT_BOLD);
+    m_itemTitle->SetFont(titleFont);
+    rightSizer->Add(m_itemTitle, 0, wxALL | wxEXPAND, 5);
+
+    // Add description
+    m_itemDescription = new wxStaticText(rightPanel, wxID_ANY, "Item description will appear here");
+    rightSizer->Add(m_itemDescription, 0, wxALL | wxEXPAND, 5);
+
+    rightPanel->SetSizer(rightSizer);
+    mainSizer->Add(rightPanel, 80, wxEXPAND);
+
+    SetSizer(mainSizer);
+
+    // Bind events
+    m_listBox->Bind(wxEVT_LISTBOX, &MainFrame::OnListBoxSelect, this);
+
+
+    // Enable persistence, END of MainFrame
     SetName("MainFrame");
     wxPersistentRegisterAndRestore(this, "MainFrame");
+}
+
+
+void MainFrame::OnListBoxSelect(wxCommandEvent& evt) {
+    int selection = m_listBox->GetSelection();
+    if (selection != wxNOT_FOUND) {
+        wxString itemName = m_listBox->GetString(selection);
+        m_itemTitle->SetLabel(itemName);
+
+        // Example descriptions
+        wxString description;
+        if (itemName == "Sword") {
+            description = "A sharp blade for close combat";
+        }
+        else if (itemName == "Shield") {
+            description = "Protective gear to block attacks";
+        }
+        else if (itemName == "Potion") {
+            description = "Restores 50 HP when consumed";
+        }
+        else if (itemName == "Bow") {
+            description = "Long-range weapon for precise attacks";
+        }
+        else if (itemName == "Arrow") {
+            description = "Ammunition for bow. Deals 25 damage";
+        }
+
+        m_itemDescription->SetLabel(description);
+        m_itemCount->SetLabel(wxString::Format("Quantity: %d", selection + 1));  // Example counter
+    }
 }
--- MainFrame.h
+++ MainFrame.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <wx/wx.h>
+#include <wx/listbox.h>
 
 class MainFrame : public wxFrame
 {
@@ -10,5 +11,14 @@ public:
     virtual wxString GetName() const override {
         return "MainFrame";
     }
+
+private:
+    wxListBox* m_listBox;
+    wxStaticText* m_itemCount;
+    wxStaticText* m_itemTitle;
+    wxStaticText* m_itemDescription;
+
+    void OnListBoxSelect(wxCommandEvent& evt);
+
 };
 
